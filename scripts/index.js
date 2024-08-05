f4acb5d5901fa4e88dfcc6c43f5b5ec4e67cf21d
Ezequiel Sale <ezequielsale123@gmail.com>
2024-08-05 12:54:13 -0300

    tercer commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 const axios = require("axios");
 const containerCards = require("./containerCards");
 const autoPlayVideo = require("./autoPlayVideo");
-const URI = process.env.URI
 
 const axiosGetData = async () => {
   try {
-    const { data } = await axios.get(`${URI}/movies`);
+    const { data } = await axios.get(`https://moviesapp-back.onrender.com/movies`);
     containerCards(data);
   } catch (error) {
     alert(error.message);
